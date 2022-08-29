b4025c9be12618b9d9f50ec8e02395a56221e2de
Thameem <you@example.com>
2022-08-29 16:16:07 +0530

    Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Navbar from './component/Na/Navbar/Navbar';
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Na/Navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import {Container,Row,Col} from 'react-bootstrap';
+function Navbar() {
+  return (
+    <div>
+      <Container>
+        <Row>
+          <Col md={5}>
+            <a href="index.html">
+              <img src="" alt="" />
+            </a>
+          </Col>
+          <Col md={7}>ewgEGW</Col>
+        </Row>
+      </Container>
+    </div>
+  )
+}
+
+export default Navbar
